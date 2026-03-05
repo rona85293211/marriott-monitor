2b5cda7903350d0dff24749756ae8e5414210d87
rona85293211 <rona85293211@gmail.com>
2026-03-05 18:31:45 +0800

    Update smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -3,23 +3,34 @@ import requests
 
 API_KEY = os.environ["RESEND_API_KEY"]
 
-HOTEL_URL = "https://www.marriott.com.cn/reservation/availability.mi?propertyCode=ZQZEL&checkInDate=2026-03-07&checkOutDate=2026-03-08&roomCount=1&numAdultsPerRoom=1"
+# 万豪查询链接
+URL = "https://www.marriott.com.cn/reservation/availability.mi?propertyCode=ZQZEL&checkInDate=2026-03-08&checkOutDate=2026-03-09&roomCount=1&numAdultsPerRoom=1"
 
-response = requests.get(HOTEL_URL)
+response = requests.get(URL)
 
 html = response.text.lower()
 
+# 判断是否有房
+no_room_words = [
+    "没有可用客房",
+    "no availability",
+    "sold out"
+]
+
 available = True
 
-if "sold out" in html or "没有可用客房" in html or "no availability" in html:
-    available = False
+for w in no_room_words:
+    if w in html:
+        available = False
 
+# 如果发现有房就发邮件
 if available:
+
     payload = {
         "from": "onboarding@resend.dev",
         "to": ["85293211@qq.com"],
         "subject": "万豪酒店放房提醒",
-        "text": "你监控的万豪酒店可能有房了，请尽快查看。"
+        "text": "检测到酒店可能放房，请立即查看。"
     }
 
     headers = {
@@ -33,7 +44,7 @@ if available:
         headers=headers
     )
 
-    print("发现房间，已发送邮件")
+    print("发现房间，已发送提醒")
 
 else:
-    print("暂时没有房间")
+    print("当前没有房间")
