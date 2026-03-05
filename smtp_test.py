f2d7f778c7f9f29c94422014aef936d34d6933e9
rona85293211 <rona85293211@gmail.com>
2026-03-05 17:58:19 +0800

    Update smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -1,36 +1,22 @@
+import os
+import requests
 
-import smtplib
-from email.message import EmailMessage
+API_KEY = os.environ["RESEND_API_KEY"]
 
-# ====== 在这里填写你的邮箱信息 ======
+url = "https://api.resend.com/emails"
 
-SMTP_HOST = "smtp.office365.com"      # 例如: smtp.gmail.com / smtp.qq.com
-SMTP_PORT = 587                   # Gmail/QQ 通常是 587
-SMTP_USER = "rona8529@outlook.com"
-SMTP_PASS = "121211Rona"   # Gmail App Password 或 QQ 授权码
+payload = {
+    "from": "onboarding@resend.dev",
+    "to": ["rona85293211@gmail.com"],
+    "subject": "GitHub 邮件测试",
+    "html": "<p>如果你收到这封邮件，说明 Resend 邮件系统正常。</p>"
+}
 
-MAIL_TO = "85293211@qq.com"    # 收件邮箱
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
 
+r = requests.post(url, json=payload, headers=headers)
 
-# ====== 邮件内容 ======
-
-msg = EmailMessage()
-msg["Subject"] = "SMTP Test Email"
-msg["From"] = SMTP_USER
-msg["To"] = MAIL_TO
-msg.set_content("如果你收到这封邮件，说明SMTP配置成功。")
-
-
-# ====== 发送邮件 ======
-
-try:
-    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
-        server.starttls()
-        server.login(SMTP_USER, SMTP_PASS)
-        server.send_message(msg)
-
-    print("邮件发送成功！")
-
-except Exception as e:
-    print("邮件发送失败：")
-    print(e)
+print(r.text)
